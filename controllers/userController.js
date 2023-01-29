528b263a3b0b72cb8b271243933a094261d4462d
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-29 10:06:51 +0600

    login method created done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,9 @@
 import Joi from "joi";
+import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
 import User from "../models/userModel.js";
 
 export const register = async (req, res) => {
-  console.log(req.body);
   try {
     const JoiSchema = Joi.object({
       name: Joi.string().min(3).max(250).required(),
@@ -19,7 +20,9 @@ export const register = async (req, res) => {
       });
     }
     // mongo data storing
-    const { name, email, address, phone, password } = req.body;
+    let { name, email, address, phone, password } = req.body;
+    const salt =await bcrypt.genSalt(10);
+    password=await bcrypt.hash(password,salt)
     const isExist = await User.findOne({ phone: phone });
     if (isExist) {
       res.send({
@@ -31,15 +34,42 @@ export const register = async (req, res) => {
       await user.save();
       res.send({
         success: true,
-        message: "User save successfully",
+          message: "User save successfully",
+        // password:password
       });
     }
   } catch (error) {
     console.log("from catch", error);
   }
 };
-const login = async (req, res) => {
+// login method
+export const login = async (req, res) => {
   try {
+        const { phone, password } = req.body;
+        const user = await User.findOne({ phone });
+        if (!user) {
+            res.status(404).send("Account not found")
+        }
+    const verifyPassword =await bcrypt.compare(password, user.password);
+        if (!verifyPassword) {
+          res.status(406).send({
+            success: false,
+            message: "INVALID PIN",
+            error:verifyPassword
+            })
+    }
+    else{
+        // token generation part
+        const tokenCredential = {
+            name: user.name,
+            phone:user.phone
+        }
+        const token = jsonwebtoken.sign(tokenCredential, process.env.TOKEN_SECRET, { expiresIn: '1m' });
+        res.send({
+            success: true,
+            accessToken:token
+        })
+      }
   } catch (error) {
     console.log(error);
   }
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.config.js';
-import routes from './routes/index.js'
+import routes from './routes/index.js';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env' })
 
 
-const server=express()
+const server = express()
 server.use(cors());
 server.use(express.json());
 
